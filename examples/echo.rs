e4928cfae3f29d4eb5102c5248de54fab052c449
Eduard Bopp <eduard.bopp@aepsil0n.de>
2016-02-14 14:02:16 +0100

    Reformat echo example

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,14 +1,16 @@
 extern crate carboxyl;
 extern crate carboxyl_cli;
 
-use std::io::{ stdin, stdout };
 use carboxyl::Stream;
-use carboxyl_cli::{ run, Quit, Input };
+use carboxyl_cli::{run, Quit, Input};
 
 fn echo(inputs: Stream<Input>) -> (Stream<String>, Stream<Quit>) {
-    (inputs.filter_map(Input::line), inputs.filter_map(Input::end))
+    (inputs.filter_map(Input::line),
+     inputs.filter_map(Input::end))
 }
 
 fn main() {
+    use std::io::{stdin, stdout};
+
     run(stdin(), stdout(), echo);
 }
